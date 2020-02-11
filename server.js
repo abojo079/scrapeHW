2c7670baeb9e23aaa16a0d7d559ba8024d51eae5
Aimee Bojorquez <abojo079@me.com>
2020-02-10 19:06:08 -0500

    server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongoose.connect('mongodb://abojo079:Amang0o79**@ds063177.mlab.com:63177/heroku_
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://celebrityinsider.org/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
